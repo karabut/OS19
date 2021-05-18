61ffea9eeb8218c3410cee5447fae44a2fe33317
ekaterinakarabut <kes_n@mail.ru>
2021-05-18 08:47:45 +0700

    initial

---
+++ cmake-build-debug/CMakeFiles/OS19.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OS19.dir/main.cpp.o"
+  "OS19"
+  "OS19.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OS19.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <dirent.h>
+#include <limits.h>
+#include <glob.h>
+
+#define SIZE 256
+#define NOT_END_OF_INPUT 1
+#define ERROR -1
+#define SUCCESS 0
+
+int readPattern(char *pattern) {
+    printf("Enter pattern: ");
+    int patternLength = 0;
+    while(NOT_END_OF_INPUT) {
+        char currentSymbol = (char)fgetc(stdin);
+        if (currentSymbol == EOF || currentSymbol == '\n') {
+            break;
+        }
+        if (currentSymbol == '/') {
+            fprintf(stderr, "Symbol '/' is not supported pattern\n");
+            return ERROR;
+        }
+        pattern[patternLength] = currentSymbol;
+        patternLength++;
+    }
+    if (patternLength == 0) {
+        fprintf(stderr, "the pattern can not be empty\n");
+        return ERROR;
+    }
+    pattern[patternLength] = '\0';
+    return EXIT_SUCCESS;
+}
+
+int print(char *pattern) {
+    glob_t result;
+    int flag = glob(pattern, GLOB_NOCHECK, NULL, &result);
+    if (flag != SUCCESS) {
+        perror("glob error");
+        globfree(&result);
+        return ERROR;
+    }
+    for (int i = 0; i < result.gl_pathc; i++) {
+        printf("%s\n", result.gl_pathv[i]);
+    }
+    globfree(&result);
+    return EXIT_SUCCESS;
+}
+
+int main(int argc, char* argv[]) {
+    char pattern[SIZE];
+    if (readPattern(pattern) == ERROR) {
+        free(pattern);
+        exit(EXIT_FAILURE);
+    }
+    if (print(pattern) == ERROR) {
+        free(pattern);
+        exit(EXIT_FAILURE);
+    }
+
+    free(pattern);
+    exit(EXIT_SUCCESS);
+}
+
